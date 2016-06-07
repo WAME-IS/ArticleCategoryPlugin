d5e0ba075b9fa52dffa32cd1e4ef431580d9aa5c
shadd0w <rene.gmitter@wame.sk>
2016-06-07 16:56:01 +0200

    FIX categories

---
+++ forms/CategoryTree/CategoryTreeFormContainer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wame\ArticleCategoryPlugin\Forms;
+
+use Wame\DynamicObject\Forms\BaseFormContainer;
+use Wame\CategoryModule\Repositories\CategoryRepository;
+use Wame\CategoryModule\Repositories\CategoryItemRepository;
+
+interface ICategoryTreeFormContainerFactory
+{
+	/** @return CategoryTreeFormContainer */
+	public function create();
+}
+
+class CategoryTreeFormContainer extends \Wame\CategoryModule\Forms\CategoryTreeFormContainer
+{
+	public function __construct(CategoryRepository $categoryRepository, CategoryItemRepository $categoryItemRepository, \Wame\Utils\HttpRequest $httpRequest) {
+		parent::__construct($categoryRepository, $categoryItemRepository, $httpRequest);
+		
+		$this->type = 'articles';
+	}
+	
+}
